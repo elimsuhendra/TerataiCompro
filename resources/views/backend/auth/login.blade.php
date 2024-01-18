f0ec1373ad44bfd6b3a07f57fa8aa411024cf6e7
abdulmuti12 <abdulmfmuti@gmail.com>
2024-01-18 22:02:17 +0700

    form login template

--- resources/views/backend/auth/login.blade.php
+++ resources/views/backend/auth/login.blade.php
@@ -63,38 +63,40 @@
 
                                         <div class="mt-4">
                                             <form method="POST" action="{{ route('admin.login.submit') }}">
-
+                                                @csrf
                                                 <div class="mb-3">
-                                                    <label for="username" class="form-label">Username</label>
-                                                    <input type="text" class="form-control" id="username" name="email" placeholder="Enter username">
+                                                    <label for="username" class="form-label">Email address or Username</label>
+                                                    <input type="text" class="form-control" id="username" name="email" placeholder="Enter email or username">
+                                                    @error('email')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
                                                 </div>
-
+                                            
                                                 <div class="mb-3">
-                                                    <div class="float-end">
-                                                        <a href="auth-pass-reset-cover.html" class="text-muted">Forgot password?</a>
-                                                    </div>
                                                     <label class="form-label" for="password-input">Password</label>
                                                     <div class="position-relative auth-pass-inputgroup mb-3">
-                                                        <input type="password" class="form-control pe-5 password-input" name="password" placeholder="Enter password" id="password-input">
+                                                        <input type="password" class="form-control pe-5 password-input" id="password-input" name="password" placeholder="Enter password">
                                                         <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                                     </div>
+                                                    @error('password')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
                                                 </div>
-
+                                            
                                                 <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                                                    <input class="form-check-input" type="checkbox" value="1" id="auth-remember-check" name="remember">
                                                     <label class="form-check-label" for="auth-remember-check">Remember me</label>
                                                 </div>
-
+                                            
                                                 <div class="mt-4">
-                                                    {{-- <button class="btn btn-success w-100" type="submit">Sign In</button> --}}
-                                                    <button class="btn btn-success w-100"  id="form_submit" type="submit">Sign In <i class="ti-arrow-right"></i></button>
+                                                    <button class="btn btn-success w-100" type="submit">Sign In</button>
                                                 </div>
-
+                                            
                                                 <div class="mt-4 text-center">
                                                     <div class="signin-other-title">
                                                         <h5 class="fs-13 mb-4 title">Sign In with</h5>
                                                     </div>
-
+                                            
                                                     <div>
                                                         <button type="button" class="btn btn-primary btn-icon waves-effect waves-light"><i class="ri-facebook-fill fs-16"></i></button>
                                                         <button type="button" class="btn btn-danger btn-icon waves-effect waves-light"><i class="ri-google-fill fs-16"></i></button>
@@ -102,8 +104,8 @@
                                                         <button type="button" class="btn btn-info btn-icon waves-effect waves-light"><i class="ri-twitter-fill fs-16"></i></button>
                                                     </div>
                                                 </div>
-
                                             </form>
+                                            
                                         </div>
 
                                         <div class="mt-5 text-center">
