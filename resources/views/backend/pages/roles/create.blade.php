128dd1f67b4dbb629c87fde8a8d739051cb73a49
abdulmuti12 <abdulmfmuti@gmail.com>
2024-02-02 22:38:17 +0700

    update role page

--- resources/views/backend/pages/roles/create.blade.php
+++ resources/views/backend/pages/roles/create.blade.php
@@ -15,7 +15,7 @@ Role Create - Admin Panel
 
 
 @section('admin-content')
-
+{{-- 
 <!-- page title area start -->
 <div class="page-title-area">
     <div class="row align-items-center">
@@ -106,4 +106,108 @@ Role Create - Admin Panel
 
 @section('scripts')
      @include('backend.pages.roles.partials.scripts')
+@endsection --}}
+
+
+<div class="page-content">
+    <div class="container-fluid">
+
+        <div class="row align-items-center">
+            <div class="col-sm-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">Role</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.artikels.index') }}">Role</a></li>
+                            <li class="breadcrumb-item active">Tambah Data</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-header align-items-center d-flex">
+                        <h4 class="card-title mb-0 flex-grow-1">Tambah Role</h4>
+                    </div><!-- end card header -->
+                    <div class="card-body">
+
+                        <h4 class="header-title">Create New Role</h4>
+                        @include('backend.layouts.partials.messages')
+                        
+                        <form action="{{ route('admin.roles.store') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Role Name</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Enter a Role Name">
+                            </div>
+    
+                            <div class="form-group">
+                                <label for="name">Permissions</label>
+    
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="checkPermissionAll" value="1">
+                                    <label class="form-check-label" for="checkPermissionAll">All</label>
+                                </div>
+                                <hr>
+                                @php $i = 1; @endphp
+                                @foreach ($permission_groups as $group)
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="{{ $i }}Management" value="{{ $group->name }}" onclick="checkPermissionByGroup('role-{{ $i }}-management-checkbox', this)">
+                                                <label class="form-check-label" for="checkPermission">{{ $group->name }}</label>
+                                            </div>
+                                        </div>
+    
+                                        <div class="col-9 role-{{ $i }}-management-checkbox">
+                                            @php
+                                                $permissions = App\User::getpermissionsByGroupName($group->name);
+                                                $j = 1;
+                                            @endphp
+                                            @foreach ($permissions as $permission)
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" name="permissions[]" id="checkPermission{{ $permission->id }}" value="{{ $permission->name }}">
+                                                    <label class="form-check-label" for="checkPermission{{ $permission->id }}">{{ $permission->name }}</label>
+                                                </div>
+                                                @php  $j++; @endphp
+                                            @endforeach
+                                            <br>
+                                        </div>
+    
+                                    </div>
+                                    @php  $i++; @endphp
+                                @endforeach
+    
+                                
+                            </div>
+                           
+                            
+                            <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">Save Role</button>
+                        </form>                    </div>
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+
+    </div>
+</div>
+
+@endsection
+
+@section('scripts')
+@include('backend.pages.roles.partials.scripts')
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+<script>
+
+    $(document).ready(function() {
+        $('.select2').select2();
+    })
+</script>
 @endsection
