af4b5c5ef63d55851c12b7f12f96a3a93cc13e6d
elimsuhendra <elimsuhendra@gmail.com>
2024-02-20 10:00:30 +0700

    edit home

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -15,6 +15,9 @@ class HomeController extends Controller
     public function __construct()
     {
         // $this->middleware('auth');
+        $this->serial_category_hidroponik = '123321';
+        $this->serial_category_cafe = '123324';
+        $this->serial_category_edufarm = '123326';
     }
 
     public function redirectAdmin()
@@ -30,7 +33,38 @@ class HomeController extends Controller
     public function index()
     {
         $title="Dashboard";
+
+        $this->set_arr_where('name','Slide');
+        $data['slide'] = $this->get_index('homes','');
+
+
+        $data['top_product_hidroponik'] = DB::table('produk')
+                                ->join('kategori','kategori.serial','produk.serial_kategori')
+                                ->where([['produk.status','Active'],['kategori.parent_serial',$this->serial_category_hidroponik]])
+                                ->select('produk.*','kategori.nama_kategori')
+                                ->limit(3)
+                                ->get();    
+
+        $data['top_product_cafe'] = DB::table('produk')
+                                ->join('kategori','kategori.serial','produk.serial_kategori')
+                                ->where([['produk.status','Active'],['kategori.parent_serial',$this->serial_category_cafe]])
+                                ->select('produk.*','kategori.nama_kategori')
+                                ->limit(3)
+                                ->get();
+
+        $data['top_product_edufarm'] = DB::table('produk')
+                                ->join('kategori','kategori.serial','produk.serial_kategori')
+                                ->where([['produk.status','Active'],['kategori.parent_serial',$this->serial_category_edufarm]])
+                                ->select('produk.*','kategori.nama_kategori')
+                                ->limit(3)
+                                ->get(); 
+
+        // dd($data['top_product_edufarm']);
+
         $data['page'] = 'home';
+
+        $data['image_url'] = $this->image_url;
+
         return view('front/home', compact('data','title'));
     }
 
